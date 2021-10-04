36fef98d9d132194713d7d73ad3ae41def737d02
seondal <sseondal@gmail.com>
2021-10-04 21:44:59 +0900

    [Feat]  회원가입 화면에서 비밀번호 표시를 누를 경우 비밀번호가 표시되도록 구현하기

--- DalTube/DalTube/SigninViewController.swift
+++ DalTube/DalTube/SigninViewController.swift
@@ -10,14 +10,30 @@ import UIKit
 class SigninViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var showPasswordButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
+
+    @IBAction func didTapShowPassword(_ sender: UIButton) {
+        if passwordTextField.isSecureTextEntry {
+            showPasswordButton.setImage(UIImage(systemName: "checkmark.rectangle.fill"), for: .normal)
+            passwordTextField.isSecureTextEntry = false
+
+        } else {
+            passwordTextField.isSecureTextEntry = true
+            showPasswordButton.setImage(UIImage(systemName: "rectangle"), for: .normal)
+        }
+    }
+    
     @IBAction func touchUpToGoToWelcomeView(_ sender: Any) {
         guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "WelcomeViewController") as? WelcomeViewController else {return}
         
