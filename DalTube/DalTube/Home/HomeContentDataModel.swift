9a267c9eee616d89f5d6954cf9ca029a273e1782
seondal <sseondal@gmail.com>
2021-11-16 22:24:29 +0900

    #5 ➕ ContentData 구조체 생성

---
+++ DalTube/DalTube/Home/HomeContentDataModel.swift
@@ -0,0 +1,24 @@
+//
+//  HomeContentDataModel.swift
+//  DalTube
+//
+//  Created by 김선오 on 2021/11/16.
+//
+
+import UIKit
+
+struct HomeContentData {
+    
+    let title: String
+    let detail: String
+    let authorImageName: String
+    let thumbImageName: String
+    
+    func makeThumbImage() -> UIImage? {
+        return UIImage(named: thumbImageName)
+    }
+    
+    func makeAuthorImage() -> UIImage? {
+        return UIImage(named: authorImageName)
+    }
+}
