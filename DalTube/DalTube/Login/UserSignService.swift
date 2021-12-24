c72db986d80364982da38035b20d006acc5be984
seondal <sseondal@gmail.com>
2021-12-24 14:08:04 +0900

    #7 로그인 통신때 사용할 함수 정의

---
+++ DalTube/DalTube/Login/UserSignService.swift
@@ -0,0 +1,27 @@
+//
+//  UserSignService.swift
+//  DalTube
+//
+//  Created by 김선오 on 2021/12/24.
+//
+
+import Foundation
+import Alamofire
+
+struct UserSignService {
+    static let shared = UserSignService()
+    
+    func login(email:String, password: String, completion: @escaping (NetworkResult<Any>) -> (Void)){
+        
+        let url = APIConstants.loginURL
+        
+        let header : HTTPHeaders = [
+            "Content-Type" : "application/json"
+        ]
+        
+        let body : Parameters = [
+            "email" : email,
+            "password" : password
+        ]
+    }
+}
